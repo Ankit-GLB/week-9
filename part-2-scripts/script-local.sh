19d2d75e9563ef85327fcc3ee2f9f976de4a0c88
Ankit-GLB <ankitraj.ms11@gmail.com>
2023-11-12 00:14:19 +0530

    added the action for deployment

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "todo-app-test-1-key.pem" ubuntu@ec2-3-111-214-192.ap-south-1.compute.amazonaws.com  "sudo bash ~/deploy.sh"
